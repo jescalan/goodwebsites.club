72bac38e5c9d4401b2d83ee62efdfd00ce00675c
Jeff Escalante <jescalan@users.noreply.github.com>
2021-07-01 14:58:32 -0400

    initial commit

---
+++ pages/home/index.tsx
@@ -0,0 +1,8 @@
+export default function IndexPage() {
+  return (
+    <p>
+      Welcome to the good websites club! Cool things coming soon. Want to get
+      involved? <a href="https://twitter.com/jescalan">Drop Jeff a DM</a>
+    </p>
+  );
+}
