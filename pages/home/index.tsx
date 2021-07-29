d9d55d3b879956ec99e326b0f5c77dca590dd38e
Rico Kahler <ricokahler@me.com>
2021-07-29 12:28:16 -0400

    fix: add title

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,17 +1,23 @@
+import Head from 'next/head'
 import s from './style.module.css'
 
 export default function IndexPage() {
   return (
-    <main className={s.root}>
-      <h1>💖 Good Websites Club 💖</h1>
-      <p>
-        We are an open community of web developers aiming to build great
-        websites at any scale. Lots more goodness coming here soon. Want to get
-        involved?{' '}
-        <a href="https://join.slack.com/t/newworkspace-mqe4911/shared_invite/zt-shzvrkyb-YTxPXyvwJwFpOZJAaZES8w">
-          Join our community on slack
-        </a>
-      </p>
-    </main>
+    <>
+      <Head>
+        <title>Good Websites Club</title>
+      </Head>
+      <main className={s.root}>
+        <h1>💖 Good Websites Club 💖</h1>
+        <p>
+          We are an open community of web developers aiming to build great
+          websites at any scale. Lots more goodness coming here soon. Want to
+          get involved?{' '}
+          <a href="https://join.slack.com/t/newworkspace-mqe4911/shared_invite/zt-shzvrkyb-YTxPXyvwJwFpOZJAaZES8w">
+            Join our community on slack
+          </a>
+        </p>
+      </main>
+    </>
   )
 }
