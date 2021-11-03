826532db26df11f125408ddbc31744d1e4368c7b
Jeff Escalante <jescalan@users.noreply.github.com>
2021-11-03 15:31:59 -0400

    change link to discords

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -14,8 +14,8 @@ export default function IndexPage() {
           We are an open community of web developers aiming to build great
           websites at any scale. Lots more goodness coming here soon. Want to
           get involved?{' '}
-          <a href="https://join.slack.com/t/goodwebsitesclub/shared_invite/zt-u26d7jmd-tklVH7bWjM28dfzJVLLKSA">
-            Join our community on slack
+          <a href="https://discord.gg/8uWUGS4G7A">
+            Join our community on discord
           </a>
         </p>
       </main>
